48d8c7806ef49b04bfe5f03ca80d1b5051ca9a3f
Raikiri <donxenapo@gmail.com>
2024-10-13 16:50:06 +1300

    Shader serialization mostly complete

--- source/LegitScriptEmBindings.cpp
+++ source/LegitScriptEmBindings.cpp
@@ -11,16 +11,50 @@ namespace ls
     instance.reset(new ls::LegitScript(slider_float_func, slider_int_func, text_func));
   }
   
+  json SerializeSamplers(const std::vector<ls::ScriptShaderDesc::Sampler> samplers)
+  {
+    auto arr = json::array();
+    for(auto sampler : samplers)
+    {
+      arr.push_back(json::object({{"type", sampler.type}, {"name", sampler.name}}));
+    }
+    return arr;
+  }
+  json SerializeUniforms(const std::vector<ls::ScriptShaderDesc::Uniform> uniforms)
+  {
+    auto arr = json::array();
+    for(auto uniform : uniforms)
+    {
+      arr.push_back(json::object({{"type", uniform.type}, {"name", uniform.name}}));
+    }
+    return arr;
+  }
+  json SerializeInouts(const std::vector<ls::ScriptShaderDesc::Inouts> inouts)
+  {
+    auto arr = json::array();
+    for(auto inout : inouts)
+    {
+      arr.push_back(json::object({{"type", inout.type}, {"name", inout.name}}));
+    }
+    return arr;
+  }
   std::string OutputShaderDescsJson(const ls::ScriptShaderDescs &descs)
   {
     auto arr = json::array();
     for(auto desc : descs)
     {
-      json json_desc = {{"name", desc.name}, {"body", desc.body}};
+      json json_desc;
+      json_desc["name"] = desc.name;
+      json_desc["body"] = desc.body;
+      json_desc["samplers"] = SerializeSamplers(desc.samplers);
+      json_desc["uniforms"] = SerializeUniforms(desc.uniforms);
+      json_desc["outs"] = SerializeInouts(desc.outs);
       arr.push_back(json_desc);
     }
 
-    return std::string(arr.dump(2));
+    json shader_descs_json;
+    shader_descs_json["shader_descs"] = arr;
+    return std::string(shader_descs_json.dump(2));
   }
   
   std::string LoadScript(std::string script_source)
