404bc34ccdeb300358f2598706ff0e05308ba7d8
Elijah Insua <tmpvar@gmail.com>
2024-10-16 19:20:49 -0700

    Add sliders/text output to demo
    Figured out how to do callbacks with emscripten and wired up some basic sliders.
    I'm avoiding writing a full on immediate mode gui here because that would be silly,
    so this is a bit rough and may do weird things if you push it too hard.
    
    known:
    - sliders will not be removed
    - all textual output is put into a single container

--- web/LegitScriptEmscriptenApi.cpp
+++ web/LegitScriptEmscriptenApi.cpp
@@ -2,25 +2,23 @@
 #include <iostream>
 
 #include <emscripten/bind.h>
+#include <emscripten/val.h>
 #include <json.hpp>
 
 #include "LegitScriptJsonApi.h"
 
-std::string LegitScriptLoad(std::string source) {
-
-  ls::InitScript([](std::string name, float val, float min_val, float max_val) -> float
+std::string LegitScriptLoad(std::string source, emscripten::val imControls) {
+  ls::InitScript([imControls](std::string name, float val, float min_val, float max_val) -> float
     {
-      std::cout << "Slider int: " << val << "[" << min_val << ", " << max_val << "]\n";
-      return val;
+      return imControls.call<float>("floatSlider", name, val, min_val, max_val);
     },
-    [](std::string name, int val, int min_val, int max_val) -> int
+    [imControls](std::string name, int val, int min_val, int max_val) -> int
     {
-      std::cout << "Slider float: " << val << "[" << min_val << ", " << max_val << "]\n";
-      return val;
+      return imControls.call<int>("intSlider", name, val, min_val, max_val);
     },
-    [](std::string text) -> void
+    [imControls](std::string text) -> void
     {
-      std::cout << "Text: " << text << "\n";
+      imControls.call<void>("text", text);
     });
 
   using json = nlohmann::json;
