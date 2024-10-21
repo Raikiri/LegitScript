3198646477a420d8905dc4b992634c28b791c683
Raikiri <donxenapo@gmail.com>
2024-10-22 02:08:11 +1300

    Fixed blend modes

--- LegitScript/source/ScriptParser.cpp
+++ LegitScript/source/ScriptParser.cpp
@@ -22,7 +22,7 @@ namespace ls
         PreambleSection     <- '[' (RendergraphSection / BlendModeSection / DeclarationSection / IncludeSection / NumthreadsSection) ']'
         RendergraphSection  <- 'rendergraph'
         BlendModeSection    <- 'blendmode' ':' BlendMode
-        BlendMode           <- <'alphablend' | 'opaque' | 'additive' | 'multiplicative'>
+        BlendMode           <- <'opaque' | 'alphablend' | 'additive' | 'multiplicative'>
         DeclarationSection  <- 'declaration' ':' String
         IncludeSection      <- 'include' ':' StringArray
         NumthreadsSection   <- 'numthreads' Int3
